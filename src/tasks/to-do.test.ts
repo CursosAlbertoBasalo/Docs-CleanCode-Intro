36d8759a8d311c7075b458ebbe6075618ee672dc
Alberto Basalo <albertobasalo@hotmail.com>
2020-01-24 13:00:48 +0100

    feat: minimal bank service

--- src/tasks/to-do.test.ts
+++ src/tasks/to-do.test.ts
@@ -1,30 +1,61 @@
+/* eslint-disable max-lines-per-function */
+/* eslint-disable max-nested-callbacks */
+import { Account, BankService, Client, Transaction } from './to-do_bad';
+
 /*
-FEATURE:    Calculate the Hamming Distance between two DNA strands.
-As a:       biologist studying cell divisions
-I want to:  compare two strands of DNA and count the differences between them
-So:         I can see how many mistakes occurred.
+FEATURE:    Simulate a bank account system.
+As a:       customer
+I want to:  have an account,
+and:        make a deposit and withdraw money,
+and:        see my movements and get a balance.
+So:         I can control my finances.
 */
 
-// Scenario: we have two invalid sequences of different lengths
-/*
-describe( 'GIVEN: a Hamming Calculator with a that receives two invalid strings', () => {
+// Scenario: I do not have an account
+
+describe('GIVEN: a Bank Service', () => {
   // Arrange
-  const sut = new HammingCalculator();
-  describe('WHEN: the first strand is longer', () => {
-    const input = ['A', ''];
-    const actual = () => sut.compare(input[0], input[1]);
-    test('THEN: should throw an error', () => {
-      expect(actual) //Act
-        .toThrow(); // assert
+  const sut = new BankService();
+  const input: Client = { name: 'john' };
+  describe('WHEN: I do not have an account', () => {
+    test('THEN: should get an undefined account', () => {
+      //Act
+      const actual: Account = sut.getAccount(input);
+      const expected = undefined;
+      // assert
+      expect(actual).toEqual(expected);
+    });
+    test('THEN: should allow me to create one', () => {
+      //Act
+      const actual: Account = sut.createAccount(input);
+      const expected: Account = { name: 'john', account: 123456789, transactions: [] };
+      // assert
+      expect(actual).toEqual(expected);
     });
   });
-  describe('WHEN: the second strand is longer', () => {
-    const input = ['A', 'AC'];
-    const actual = () => sut.compare(input[0], input[1]);
-    test('THEN: should throw an error', () => {
-      expect(actual) //Act
-        .toThrow(); // assert
+  describe('WHEN: I have an account', () => {
+    test('THEN: should get my account', () => {
+      //Act
+      const actual: string = sut.getAccount(input).name;
+      const expected = input.name;
+      // assert
+      expect(actual).toEqual(expected);
+    });
+    test('THEN: should allow me to add a transaction', () => {
+      //Act
+      const transactionInput: Transaction = { type: 'DEPOSIT', amount: 100 };
+      const account: Account = sut.addTransaction(input, transactionInput);
+      const actual = account.transactions.includes(transactionInput);
+      const expected = true;
+      // assert
+      expect(actual).toEqual(expected);
+    });
+    test('THEN: should allow me to get my balance', () => {
+      //Act
+      const actual: number = sut.getBalance(input);
+      const expected = 100;
+      // assert
+      expect(actual).toEqual(expected);
     });
   });
 });
-*/
--- src/tasks/to-do_bad.ts
+++ src/tasks/to-do_bad.ts
@@ -6,3 +6,67 @@ ingresar retirar dinero
 calcular balance error si negativo
 
 */
+
+export class BankService {
+  private accounts = new Map<string, Account>();
+  constructor() {}
+
+  getAccount(client: Client): Account {
+    return this.accounts.get(client.name);
+  }
+  createAccount(client: Client): Account {
+    const newAccount = new Account();
+    newAccount.name = client.name;
+    newAccount.account = 123456789;
+    this.accounts.set(client.name, newAccount);
+    return newAccount;
+  }
+  addTransaction(client: Client, transaction: Transaction): Account {
+    const account = this.getAccount(client);
+    if (account) {
+      if (this.isValidTransaction(transaction)) {
+        this.saveTransaction(account, transaction);
+      }
+    }
+    return account;
+  }
+
+  getBalance(client: Client): number {
+    return 100;
+  }
+
+  private isValidTransaction(transaction: Transaction): boolean {
+    if (
+      transaction.type === 'DEPOSIT' ||
+      transaction.type === 'WITHDRAW' ||
+      transaction.type === 'CANCEL'
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  private saveTransaction(account: Account, transaction: Transaction): void {
+    account.transactions.push(transaction);
+  }
+}
+
+// ❌ Compose structures
+
+export class Client {
+  name: string;
+  constructor() {}
+}
+
+export class Account {
+  name: string;
+  account: number;
+  transactions: Transaction[] = [];
+  constructor() {}
+}
+
+export class Transaction {
+  type: string;
+  amount: number;
+  constructor() {}
+}
