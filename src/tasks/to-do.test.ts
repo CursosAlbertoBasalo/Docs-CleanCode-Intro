74a44f51310fa91249050f1b9978d8e916d9e307
Alberto Basalo <albertobasalo@hotmail.com>
2020-01-24 13:54:58 +0100

    feat: task defined

--- src/tasks/to-do.test.ts
+++ src/tasks/to-do.test.ts
@@ -6,8 +6,8 @@ import { Account, BankService, Client, Transaction } from './to-do_bad';
 FEATURE:    Simulate a bank account system.
 As a:       customer
 I want to:  have an account,
-and:        make a deposit and withdraw money,
-and:        see my movements and get a balance.
+and:        make a deposit or withdraw money,
+and:        and get my balance user friendly.
 So:         I can control my finances.
 */
 
@@ -15,45 +15,73 @@ So:         I can control my finances.
 
 describe('GIVEN: a Bank Service', () => {
   // Arrange
-  const sut = new BankService();
-  const input: Client = { name: 'john' };
+  const inputClient: Client = { name: 'john' };
   describe('WHEN: I do not have an account', () => {
+    let sut: BankService;
+    beforeEach(() => {
+      // Arrange
+      sut = new BankService();
+    });
     test('THEN: should get an undefined account', () => {
       //Act
-      const actual: Account = sut.getAccount(input);
+      const actual: Account = sut.getAccount(inputClient);
       const expected = undefined;
       // assert
       expect(actual).toEqual(expected);
     });
     test('THEN: should allow me to create one', () => {
       //Act
-      const actual: Account = sut.createAccount(input);
+      const actual: Account = sut.createAccount(inputClient);
       const expected: Account = { name: 'john', account: 123456789, transactions: [] };
       // assert
       expect(actual).toEqual(expected);
     });
   });
   describe('WHEN: I have an account', () => {
+    let sut: BankService;
+    beforeEach(() => {
+      // Arrange
+      sut = new BankService();
+      sut.createAccount(inputClient);
+    });
     test('THEN: should get my account', () => {
       //Act
-      const actual: string = sut.getAccount(input).name;
-      const expected = input.name;
+      const actual: string = sut.getAccount(inputClient).name;
+      const expected = inputClient.name;
       // assert
       expect(actual).toEqual(expected);
     });
     test('THEN: should allow me to add a transaction', () => {
       //Act
       const transactionInput: Transaction = { type: 'DEPOSIT', amount: 100 };
-      const account: Account = sut.addTransaction(input, transactionInput);
+      const account: Account = sut.addTransaction(inputClient, transactionInput);
       const actual = account.transactions.includes(transactionInput);
       const expected = true;
       // assert
       expect(actual).toEqual(expected);
     });
+  });
+  describe('WHEN: I have made a deposit', () => {
+    let sut: BankService;
+    beforeAll(() => {
+      // Arrange
+      sut = new BankService();
+      sut.createAccount(inputClient);
+      const transactionInput: Transaction = { type: 'DEPOSIT', amount: 100 };
+      sut.addTransaction(inputClient, transactionInput);
+    });
     test('THEN: should allow me to get my balance', () => {
       //Act
-      const actual: number = sut.getBalance(input);
+      const actual: number = sut.getBalance(inputClient);
       const expected = 100;
+      // assert
+      expect(actual).toEqual(expected);
+    });
+    test('THEN: should allow me to get an user friendly balance message', () => {
+      //Act
+      const inputBalance = sut.getBalance(inputClient);
+      const actual: string = sut.getUserFriendlyBalanceMessage(inputBalance);
+      const expected = 'Good! you have a lot of money.';
       // assert
       expect(actual).toEqual(expected);
     });
--- src/tasks/to-do_bad.ts
+++ src/tasks/to-do_bad.ts
@@ -1,14 +1,9 @@
-// ❌
-
-/*
-
-ingresar retirar dinero
-calcular balance error si negativo
-
-*/
+// ❌ refactor and change the tests accordingly
 
 export class BankService {
+  private readonly INITIAL_BALANCE: number = 0;
   private accounts = new Map<string, Account>();
+
   constructor() {}
 
   getAccount(client: Client): Account {
@@ -23,8 +18,10 @@ export class BankService {
   }
   addTransaction(client: Client, transaction: Transaction): Account {
     const account = this.getAccount(client);
+    // ❌ not related to data but... can you reduce nesting?
     if (account) {
       if (this.isValidTransaction(transaction)) {
+        this.roundTransaction(transaction);
         this.saveTransaction(account, transaction);
       }
     }
@@ -32,19 +29,55 @@ export class BankService {
   }
 
   getBalance(client: Client): number {
-    return 100;
+    let balance = this.INITIAL_BALANCE;
+    const account = this.getAccount(client);
+    if (account) {
+      balance = this.executeTransactions(account);
+    }
+    return balance;
+  }
+  getUserFriendlyBalanceMessage(balance: number): string {
+    // ❌ reduce conditionals
+    if (balance < this.INITIAL_BALANCE) {
+      return 'Be careful with your debts.';
+    } else if (balance == this.INITIAL_BALANCE) {
+      return 'Bad luck you have no money.';
+    } else {
+      return 'Good! you have a lot of money.';
+    }
+  }
+
+  private executeTransactions(account: Account): number {
+    return account.transactions.reduce(this.executeTransaction, this.INITIAL_BALANCE);
+  }
+  private executeTransaction(accumulator: number, transaction: Transaction): number {
+    // ❌ replace switches
+    switch (transaction.type) {
+      case 'DEPOSIT':
+        return accumulator + transaction.amount;
+      case 'WITHDRAW':
+        return accumulator - transaction.amount;
+      default:
+        return accumulator;
+    }
   }
 
   private isValidTransaction(transaction: Transaction): boolean {
+    // ❌ reduce conditionals
     if (
       transaction.type === 'DEPOSIT' ||
       transaction.type === 'WITHDRAW' ||
       transaction.type === 'CANCEL'
     ) {
-      return true;
+      const MINIMAL_AMOUNT = 0;
+      return transaction.amount >= MINIMAL_AMOUNT;
     } else {
       return false;
     }
+  }
+  private roundTransaction(transaction: Transaction): void {
+    const CENTS = 100;
+    transaction.amount = Math.round(transaction.amount * CENTS) / CENTS;
   }
   private saveTransaction(account: Account, transaction: Transaction): void {
     account.transactions.push(transaction);
@@ -52,6 +85,7 @@ export class BankService {
 }
 
 // ❌ Compose structures
+// ❌ Use constructors when appropiated
 
 export class Client {
   name: string;
