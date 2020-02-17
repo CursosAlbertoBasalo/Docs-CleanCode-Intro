a36c4a1b1036d2d0c48b999279352b1f6d38fe08
Alberto Basalo <albertobasalo@hotmail.com>
2020-02-17 12:44:08 +0100

    🏗 : structures

--- src/examples/1-structures/structures_better.ts
+++ src/examples/1-structures/structures_better.ts
@@ -1,6 +1,51 @@
+// ✔️✔️ Rich data structure
+type Money = { amount: number; currency?: string };
+
+type MoneyConditionData = { principal: Money; rate: number; years: number };
+
+// ✔️✔️ validated inmutable value object
+class BankConditions {
+  public readonly principal: Money;
+  public readonly rate: number;
+  public readonly years: number;
+
+  constructor(data: MoneyConditionData) {
+    this.principal = data.principal;
+    this.rate = data.rate;
+    this.years = data.years;
+    this.validate();
+  }
+
+  getData(): MoneyConditionData {
+    return { principal: this.principal, rate: this.rate, years: this.years };
+  }
+
+  toString(): string {
+    return `${this.principal.amount}${this.principal.currency} at ${this.rate}% for ${this.years} years`;
+  }
+
+  private validate(): void {
+    const MIN_AMOUNT = 10;
+    if (this.principal.amount <= MIN_AMOUNT)
+      throw new Error(`Minimal amount o capital is ${MIN_AMOUNT}`);
+    const MIN_RATE = 0;
+    const MAX_RATE = 10;
+    if (this.rate <= MIN_RATE || this.rate > MAX_RATE)
+      throw new Error(`Rate must be positive and less than ${MAX_RATE}`);
+    const MIN_YEARS = 0;
+    const MAX_YEARS = 30;
+    if (this.rate <= MIN_YEARS || this.rate > MAX_YEARS)
+      throw new Error(`Years must be positive and less than ${MAX_YEARS}`);
+  }
+}
+
 // ✔️ object value with validations
 export function calculateInterest(): number {
-  const capitalConditions = new BankConditions({ principal: 1000, rate: 3.5, years: 1 });
+  const capitalConditions = new BankConditions({
+    principal: { amount: 1000, currency: '$' },
+    rate: 3.5,
+    years: 1,
+  });
 
   const amount = getSimpleInterest(capitalConditions);
   return amount;
@@ -8,27 +53,6 @@ export function calculateInterest(): number {
 // ✔️✔️ typed inmutable single parameter
 function getSimpleInterest(conditions: BankConditions): number {
   const PER_CENT = 100;
-  const amount = (conditions.principal * conditions.rate * conditions.years) / PER_CENT;
+  const amount = (conditions.principal.amount * conditions.rate * conditions.years) / PER_CENT;
   return amount;
-}
-
-// ✔️✔️ validated inmutable value object
-class BankConditions {
-  public readonly principal: number;
-  public readonly rate: number;
-  public readonly years: number;
-
-  constructor(initial: { principal: number; rate: number; years: number }) {
-    this.principal = initial.principal;
-    this.rate = initial.rate;
-    this.years = initial.years;
-    this.validate();
-  }
-
-  private validate(): void {
-    const MIN_RATE = 0;
-    const MAX_RATE = 20;
-    if (this.rate <= MIN_RATE || this.rate > MAX_RATE)
-      throw new Error('Rate must be positive and less than 20');
-  }
 }
--- src/examples/1-structures/structures_ok.ts
+++ src/examples/1-structures/structures_ok.ts
@@ -1,16 +1,16 @@
+// ✔️ in typescript
+type MoneyConditionData = { principal: number; rate: number; years: number };
+
 // ✔️ object literal
 export function calculateInterest(): number {
-  const capitalConditions = { principal: 1000, rate: 3.5, years: 1 };
+  const capitalConditions: MoneyConditionData = { principal: 1000, rate: 3.5, years: 1 };
 
   const amount = getSimpleInterest(capitalConditions);
   return amount;
 }
 // ✔️ single parameter
-function getSimpleInterest(conditions: BankCondition): number {
+function getSimpleInterest(conditions: MoneyConditionData): number {
   const PER_CENT = 100;
   const amount = (conditions.principal * conditions.rate * conditions.years) / PER_CENT;
   return amount;
 }
-
-// ✔️ in typescript
-type BankCondition = { principal: number; rate: number; years: number };
