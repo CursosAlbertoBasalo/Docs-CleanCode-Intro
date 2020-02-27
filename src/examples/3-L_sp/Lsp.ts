d573ffd63de0efbcaf4f554a5179d005822472bd
Alberto Basalo <albertobasalo@hotmail.com>
2020-02-27 16:44:50 +0100

    ✨: ready Isp

---
+++ src/examples/3-L_sp/Lsp.ts
@@ -0,0 +1,79 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+class Shape {
+  constructor(public readonly kind: string) {}
+}
+export class Triangle extends Shape {
+  constructor(public readonly base: number, public readonly height: number) {
+    super('Triangle');
+    this.validate();
+  }
+  private validate(): void {
+    if (this.base <= 0 || this.height <= 0) {
+      throw 'Both measures must be positive.';
+    }
+  }
+}
+// ❌ a subtype with very particular characteristics
+export class RightTriangle extends Triangle {
+  constructor(public readonly base: number, public readonly height: number) {
+    super(base, height);
+  }
+}
+
+export class Square extends Shape {
+  constructor(public readonly side: number) {
+    super('Square');
+    this.validate();
+  }
+  private validate(): void {
+    if (this.side <= 0) {
+      throw 'Side must be positive.';
+    }
+  }
+}
+
+// ❌ inheritance may hide problems
+export abstract class Calculator {
+  abstract getArea(shape: Shape): number;
+  abstract getPerimeter(shape: Shape): number;
+}
+// ✔️ at least we are covered
+export class TriangleCalculator extends Calculator {
+  getArea(triangle: Triangle): number {
+    const HALVE = 0.5;
+    return HALVE * triangle.base * triangle.height;
+  }
+  getPerimeter(triangle: Triangle): number {
+    throw 'Perimeter cannot always be inferred from every triangle.';
+  }
+}
+
+// ✔️ overridden methods may avoid the base case
+export class RightTriangleCalculator extends TriangleCalculator {
+  getPerimeter(triangle: RightTriangle): number {
+    const hypotenuse = Math.sqrt(triangle.base * triangle.base + triangle.height * triangle.height);
+    return triangle.base + triangle.height + hypotenuse;
+  }
+}
+
+export class SquareCalculator extends Calculator {
+  getArea(square: Square): number {
+    return square.side * square.side;
+  }
+  getPerimeter(square: Square): number {
+    const sides = 4;
+    return sides * square.side;
+  }
+}
+
+export class TrianglesRepository {
+  save(triangle: Triangle): void {
+    console.log(`Triangle with base ${triangle.base} and height ${triangle.height}`);
+  }
+}
+
+export class SquaresRepository {
+  save(square: Square): void {
+    console.log(`Square with side ${square.side}`);
+  }
+}
--- src/examples/4-I_sp/Isp.test.ts
+++ src/examples/4-I_sp/Isp.test.ts
@@ -1,19 +1,55 @@
-/* eslint-disable max-lines-per-function */
-import { Hen, Mouse } from './Isp_ok';
-
+/* eslint-disable no-magic-numbers */
 /* eslint-disable max-nested-callbacks */
+// import { Triangle } from './Lsp_bad';
+import {
+  RightTriangleCalculator,
+  Sphere,
+  SphereCalculator,
+  Square,
+  SquareCalculator,
+  Triangle,
+  TriangleCalculator,
+} from './Isp_ok';
 
-describe('Caring animals and their offspring', () => {
-  test('Given a female mouse I hope her can give birth ', () => {
-    const actual = new Mouse('Minnie').giveBirth;
-    expect(actual).toBeDefined();
+describe('work with triangles', () => {
+  test('Given a triangle get its area', () => {
+    const input = new Triangle(15, 20);
+    const actual = new TriangleCalculator().getArea(input);
+    const expected = 150;
+    expect(actual).toEqual(expected);
   });
-  test('Given a female mouse I hope her can feed her children ', () => {
-    const actual = new Mouse('Minnie').breastFeed;
-    expect(actual).toBeDefined();
+  test('Given a triangle get its Perimeter', () => {
+    const input = new Triangle(15, 20);
+    const actual = new RightTriangleCalculator().getPerimeter(input);
+    const expected = 60;
+    expect(actual).toEqual(expected);
   });
-  test('Given a female hen I hope can lay eggs ', () => {
-    const actual = new Hen('Camilla').layEggs;
-    expect(actual).toBeDefined();
+});
+describe('work with squares', () => {
+  test('Given a square get its area', () => {
+    const input = new Square(15);
+    const actual = new SquareCalculator().getArea(input);
+    const expected = 225;
+    expect(actual).toEqual(expected);
+  });
+  test('Given a square get its Perimeter', () => {
+    const input = new Square(15);
+    const actual = new SquareCalculator().getPerimeter(input);
+    const expected = 60;
+    expect(actual).toEqual(expected);
+  });
+});
+describe('work with Sphere', () => {
+  test('Given a Sphere get its area', () => {
+    const input = new Square(15);
+    const actual = new SquareCalculator().getArea(input);
+    const expected = 225;
+    expect(actual).toEqual(expected);
+  });
+  test('Given a Sphere get its Volume', () => {
+    const input = new Sphere(10);
+    const actual = Math.round(new SphereCalculator().getVolume(input));
+    const expected = 4176;
+    expect(actual).toEqual(expected);
   });
 });
