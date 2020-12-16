58982fa23501e1ec6e97533faf7e52a0eb5a9b8b
Alberto Basalo <albertobasalo@hotmail.com>
2020-12-16 11:42:58 +0100

    refactor: structures

--- src/examples/1-structures/structures_better.ts
+++ src/examples/1-structures/structures_better.ts
@@ -25,34 +25,49 @@ class BankConditions {
   }
 
   private validate(): void {
-    const MIN_AMOUNT = 10;
-    if (this.principal.amount <= MIN_AMOUNT)
-      throw new Error(`Minimal amount o capital is ${MIN_AMOUNT}`);
-    const MIN_RATE = 0;
-    const MAX_RATE = 10;
-    if (this.rate <= MIN_RATE || this.rate > MAX_RATE)
-      throw new Error(`Rate must be positive and less than ${MAX_RATE}`);
+    this.validateAmount();
+    this.validateRate();
+    this.validateYears();
+  }
+
+  private validateYears() {
     const MIN_YEARS = 0;
     const MAX_YEARS = 30;
     if (this.rate <= MIN_YEARS || this.rate > MAX_YEARS)
       throw new Error(`Years must be positive and less than ${MAX_YEARS}`);
   }
+
+  private validateRate() {
+    const MIN_RATE = 0;
+    const MAX_RATE = 10;
+    if (this.rate <= MIN_RATE || this.rate > MAX_RATE)
+      throw new Error(`Rate must be positive and less than ${MAX_RATE}`);
+  }
+
+  private validateAmount() {
+    const MIN_AMOUNT = 10;
+    if (this.principal.amount <= MIN_AMOUNT)
+      throw new Error(`Minimal amount o capital is ${MIN_AMOUNT}`);
+  }
 }
 
-// ✔️ object value with validations
 export function calculateInterest(): number {
+  // ✔️ object value with validations
   const capitalConditions = new BankConditions({
     principal: { amount: 1000, currency: '$' },
     rate: 3.5,
     years: 1,
   });
 
-  const amount = getSimpleInterest(capitalConditions);
-  return amount;
+  const interest = getSimpleInterest(capitalConditions);
+
+  return interest;
 }
 // ✔️✔️ typed inmutable single parameter
 function getSimpleInterest(conditions: BankConditions): number {
   const PER_CENT = 100;
-  const amount = (conditions.principal.amount * conditions.rate * conditions.years) / PER_CENT;
-  return amount;
+
+  const interest = (conditions.principal.amount * conditions.rate * conditions.years) / PER_CENT;
+
+  return interest;
 }
--- src/examples/1-structures/structures_ok.ts
+++ src/examples/1-structures/structures_ok.ts
@@ -1,16 +1,17 @@
 // ✔️ in typescript
 type MoneyConditionData = { principal: number; rate: number; years: number };
 
-// ✔️ object literal
 export function calculateInterest(): number {
+  // ✔️ object literal
   const capitalConditions: MoneyConditionData = { principal: 1000, rate: 3.5, years: 1 };
 
-  const amount = getSimpleInterest(capitalConditions);
-  return amount;
+  // ✔️ single parameter
+  const interest = getSimpleInterest(capitalConditions);
+  return interest;
 }
-// ✔️ single parameter
+
 function getSimpleInterest(conditions: MoneyConditionData): number {
   const PER_CENT = 100;
-  const amount = (conditions.principal * conditions.rate * conditions.years) / PER_CENT;
-  return amount;
+  const interest = (conditions.principal * conditions.rate * conditions.years) / PER_CENT;
+  return interest;
 }
